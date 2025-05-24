4261b05188d0924b4b4c4cd97f720293dfbe0b62
Shen2806 <dmvuong12a2@gmail.com>
2025-05-24 17:35:42 +0700

    video8 chapter2

---
+++ video8/video8.js
@@ -0,0 +1,11 @@
+console.log("Hello, world!");
+
+let obj = {
+  name: "Video 8",
+  description: "This is a simple JavaScript object.",
+  version: 1.0,
+};
+
+console.log("Object Name:", obj.name);
+console.log("Object Description:", obj.description);
+console.log("Object Version:", obj.version);
